f4bf64f23035332e44eb337f9e97fe4356eccbf0
nemodaattila <nemodaattila@gmail.com>
2024-04-17 15:19:29 +0200

    added Company filesToInclude.js

---
+++ frontend/modules/companies/filesToInclude.js
@@ -0,0 +1,17 @@
+Includer.setIncludableFileSources(
+    'companyLister',[
+        {
+            directory: MODULE_FILE_DIR+'/companies',
+            fileNames: [
+                'CompanyService.js',
+            ]
+        },
+        {
+        directory: MODULE_FILE_DIR+'/companies/CompanyLister',
+        fileNames: [
+            'CompanyListerController.js',
+            // 'CompanyListerView.js',
+            // 'CompanyListerModel.js',
+            // 'CompanyLister.css'
+        ]
+    }])
