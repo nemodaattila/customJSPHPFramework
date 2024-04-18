a11f6aaee79d383ae51b73f2c7c5a7fd2dd2f3ce
nemodaattila <nemodaattila@gmail.com>
2024-04-18 16:33:55 +0200

    added RequestParameters.php

---
+++ backend/model/RequestParameters.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace classModel;
+
+/**
+ * Class RequestParameters stores data originating from http requests
+ * @package core\backend\model
+ */
+class RequestParameters
+{
+    /**
+     * @var array request parameters from url
+     */
+    private array $urlParameters;
+    /**
+     * @var array parameters from request body
+     */
+    private array $requestData;
+
+    /**
+     * adds an url parameter
+     * @param string $urlParameter parameter
+     */
+    public function addUrlParameter(string $urlParameter): void
+    {
+        $this->urlParameters[] = $urlParameter;
+    }
+
+    public function getUrlParameters(): array
+    {
+        return $this->urlParameters;
+    }
+
+    public function getRequestData(): array
+    {
+        return $this->requestData;
+    }
+
+    public function setRequestData(array $requestData): void
+    {
+        $this->requestData = $requestData;
+    }
+
+    /**
+     * resetting parameters to empty
+     */
+    public function reset()
+    {
+        $this->urlParameters = [];
+        $this->requestData = [];
+    }
+
+}
