3d9f914a575ff4802a0c64cf13cb52a786c73995
nemodaattila <nemodaattila@gmail.com>
2024-04-18 15:42:00 +0200

    autoloader files

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2864720f66290ce5bd383add697b96382bdc2ade',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2864720f66290ce5bd383add697b96382bdc2ade',
+    ),
+  ),
+);
