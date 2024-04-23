91002cb2888489586363ed40eb20851ab78895c3
nemodaattila <nemodaattila@gmail.com>
2024-04-23 15:45:19 +0200

    CompanyServiceModel.js nyitóparaméterek megadása

--- frontend/modules/companies/CompanyService.js
+++ frontend/modules/companies/CompanyService.js
@@ -5,6 +5,7 @@ class CompanyService extends ServiceParent {
     }
 
     async init() {
+        console.log('init')
         return new Promise(async (resolve) => {
             if (this._model.loaded)
                 resolve(true)
@@ -18,136 +19,7 @@ class CompanyService extends ServiceParent {
         this._model.loaded = true
     }
 
-    // static requiredFiles = {
-    //     listAllCompany: 'MC',
-    //     addNewCompany: 'MC',
-    //     editMultipleCompany: 'MC',
-    //     detailedCompany: 'MC',
-    // }
-    // static moduleGroups = {
-    //     listAllCompany: ['listAllCompany'],
-    //     addNewCompany: ['addNewCompany'],
-    //     manageCompany: ['editCompany', 'companyContact', "companySystems", "companyBills", "companyWorksheets", "companyDeliveryNotes", 'companyEvents'],
-    //     editMultipleCompany: ['editMultipleCompany']
-    // }
-    // static  sideMenuParams = {
-    //     listAllCompany: {
-    //         label: 'Cégek',
-    //         openParams: {
-    //             module: 'Company',
-    //             task: 'listAllCompany',
-    //         }
-    //     },
-    //     addNewCompany: {
-    //         label: 'Cég felvétele',
-    //         openParams: {
-    //             module: 'Company',
-    //             task: 'addNewCompany',
-    //         }
-    //     },
-    //     editMultipleCompany: {
-    //         label: 'Több cég együttes szerkesztése',
-    //         openParams: {
-    //             module: 'Company',
-    //             task: 'editMultipleCompany',
-    //         }
-    //     },
-    //     editCompany: {
-    //         label: 'Cég szerkesztése',
-    //         openParams: {
-    //             module: 'Company',
-    //             task: 'detailedCompany',
-    //         }
-    //     },
-    //     companyContact: {
-    //         label: 'Cég kontaktok',
-    //         openParams: {
-    //             module: 'Contact',
-    //             task: 'listAllContact',
-    //             params: {
-    //                 tableSearchParams: [{
-    //                     type: 'company',
-    //                     value: 'connectedObjectId',
-    //                     operator: 'eq'
-    //                 }],
-    //             }
-    //         }
-    //     },
-    //     companySystems: {
-    //         label: 'Céghez tartozó rendszerek',
-    //         openParams: {
-    //             module: 'System',
-    //             task: 'listAllSystem',
-    //             params: {
-    //                 tableSearchParams: [{
-    //                     type: 'company',
-    //                     value: 'connectedObjectId',
-    //                     operator: 'eq'
-    //                 }],
-    //             }
-    //         }
-    //     },
-    //     companyBills: {
-    //         label: 'Céghez tartozó számlák',
-    //         openParams: {
-    //             module: 'Bill',
-    //             task: 'listAllBill',
-    //             params: {
-    //                 tableSearchParams: [{
-    //                     type: 'company',
-    //                     value: 'connectedObjectId',
-    //                     operator: 'eq'
-    //                 }],
-    //             }
-    //         }
-    //     },
-    //     companyWorksheets: {
-    //         label: 'Céghez tartozó munkalapok',
-    //         openParams: {
-    //             module: 'Worksheet',
-    //             task: 'listAllWorksheet',
-    //             params: {
-    //                 tableSearchParams: [{
-    //                     type: 'company',
-    //                     value: 'connectedObjectId',
-    //                     operator: 'eq'
-    //                 }],
-    //             }
-    //         }
-    //     },
-    //     companyDeliveryNotes: {
-    //         label: 'Céghez tartozó Szállítólevelek',
-    //         openParams: {
-    //             module: 'DeliveryNote',
-    //             task: 'listAllDeliveryNote',
-    //             params: {
-    //                 tableSearchParams: [{
-    //                     type: 'company',
-    //                     value: 'connectedObjectId',
-    //                     operator: 'eq'
-    //                 }],
-    //             }
-    //         }
-    //     },
-    //     companyEvents: {
-    //         label: 'Cégesemények',
-    //         openParams: {
-    //             module: 'Event',
-    //             task: 'listAllEvent',
-    //             params: {
-    //                 tableSearchParams: [{
-    //                     type: 'elementGroup',
-    //                     value: 1,
-    //                     operator: 'eq'
-    //                 }, {
-    //                     type: 'element',
-    //                     value: 'connectedObjectId',
-    //                     operator: 'eq'
-    //                 }],
-    //             }
-    //         }
-    //     }
-    // }
+
     // /**
     //  * cég-típusok pl: viszonteladó
     //  * @type {Object | null}
@@ -162,7 +34,7 @@ class CompanyService extends ServiceParent {
     //  * kiválasztott rekord
     //  * @type {Object|null}
     //  */
-    // static selectedRecord = null
+
     // static getUrl = 'getCompanies'
     //
     //
--- frontend/modules/companies/CompanyServiceModel.js
+++ frontend/modules/companies/CompanyServiceModel.js
@@ -1,7 +1,92 @@
 class CompanyServiceModel extends ServiceModelParent {
     _companyTypes = {0: 'Nem vevő'}
     set companyTypes(types) {
+        console.log(types)
         Object.values(types).forEach(type =>
             this._companyTypes[type.id] = type.name)
+    }
+
+    _selectedId
+
+    selectedRecord = null
+
+    _moduleParams = {
+        list: {
+            'module': 'companyLister',
+            'title': 'Cégek'
+        },
+        edit: {
+            'module': 'companyEditor',
+            'title': 'Cég szerkesztése',
+            'windowName': 'companyEditor'
+        },
+        multiEdit: {
+            'module': 'multipleCompanyEditor',
+            'title': 'Cégek szerkesztése'
+        },
+        add: {
+            'module': 'companyAdder',
+            'title': 'Cég felvétele'
+        },
+        deletable: true
+    }
+    restName = 'company'
+    entityTriggerName = 'company'
+    onClickEventAttribs = ['id', 'name']
+    defaultOrder = 'name'
+    attributes = {
+        id: {label: 'Azonosító', type: 'string'},
+        name: {label: 'Név', type: 'string'},
+        address: {label: 'cím', type: 'string'},
+        vat_number: {label: 'Adószám'},
+        category: {
+            label: 'Kapcsolat-típus', filterType: 'select',
+            values: this._companyTypes
+        },
+        comment: {label: 'Megjegyzés'}
+    }
+    editSubMenuParams = {
+        editCompany: {
+            label: 'Cég szerkesztése',
+            moduleGroupName: 'companies',
+            task: 'companyEditor',
+            windowName: 'companyEditor'
+        },
+        companyBills: {
+            label: 'Céghez tartozó számlák',
+            moduleGroupName: 'bills',
+            task: 'billLister',
+            windowName: 'companyEditor',
+            tableSearchParams: [{
+                type: 'company',
+                value: this._selectedId,
+                operator: 'eq'
+            }],
+        },
+        companyWorksheets: {
+            label: 'Céghez tartozó munkalapok',
+            windowName: 'companyEditor',
+            module: 'worksheets',
+            task: 'worksheetLister',
+            tableSearchParams: [{
+                type: 'company',
+                value: this._selectedId,
+                operator: 'eq'
+            }],
+        },
+        companyEvents: {
+            label: 'Cégesemények',
+            module: 'events',
+            task: 'eventLister',
+            tableSearchParams: [{
+                type: 'elementGroup',
+                value: 1,
+                operator: 'eq'
+            }, {
+                type: 'element',
+                value: this._selectedId,
+                operator: 'eq'
+            }],
+        }
     }
 }
