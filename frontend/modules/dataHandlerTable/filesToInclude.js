9efd820960cb83f86762d0c859c6a5217b628345
nemodaattila <nemodaattila@gmail.com>
2024-05-29 12:50:40 +0200

    added parameters of files to be included in dataHandlerTable filesToInclude

--- frontend/modules/dataHandlerTable/filesToInclude.js
+++ frontend/modules/dataHandlerTable/filesToInclude.js
@@ -1,39 +1,38 @@
 Includer.setIncludableFileSource(
-    'listerTable', [
-        {
-            directory: MODULE_FILE_DIR + 'dataHandlerTable',
-            fileNames: [
-                'ListerTable.js',
-                'ListerTableView.js',
-                'ListerTable.css',
-                'SearchAndOrderParameters.js',
-                'ListerControllerParent.js'
-            ]
-        }])
-Includer.setIncludableFileSource(
-    'pageTurner', [
-        {
-            directory: MODULE_FILE_DIR + 'dataHandlerTable',
-            fileNames: [
-                'PageTurnerController.js',
-                'PageTurner.css',
-            ]
-        }])
-Includer.setIncludableFileSource(
-    //DO rename
-    'listerTableSearchConnector', [
-        {
-            directory: MODULE_FILE_DIR + 'dataHandlerTable',
-            fileNames: [
-                'ListerTableSearchConnector.js',
-            ]
-        }])
-Includer.setIncludableFileSource(
-    'infinityScroller', [
-        {
-            directory: MODULE_FILE_DIR + 'dataHandlerTable',
-            fileNames: [
-                'InfinityScrollerController.js',
-                'InfinityScroller.css',
-            ]
-        }])
+    'handlerTable', [{
+        directory: MODULE_FILE_DIR + 'dataHandlerTable',
+        fileNames: [
+            'HandlerTable.js',
+            'HandlerTableView.js',
+            'HandlerTable.css',
+            // 'SearchAndOrderParameters.js',
+            'HandlerControllerParent.js'
+        ]
+    }])
+// Includer.setIncludableFileSource(
+//     'pageTurner', [
+//         {
+//             directory: MODULE_FILE_DIR + 'dataListerTable',
+//             fileNames: [
+//                 'PageTurnerController.js',
+//                 'PageTurner.css',
+//             ]
+//         }])
+// Includer.setIncludableFileSource(
+//     //DO rename
+//     'listerTableSearchConnector', [
+//         {
+//             directory: MODULE_FILE_DIR + 'dataListerTable',
+//             fileNames: [
+//                 'ListerTableSearchConnector.js',
+//             ]
+//         }])
+// Includer.setIncludableFileSource(
+//     'infinityScroller', [
+//         {
+//             directory: MODULE_FILE_DIR + 'dataListerTable',
+//             fileNames: [
+//                 'InfinityScrollerController.js',
+//                 'InfinityScroller.css',
+//             ]
+//         }])
