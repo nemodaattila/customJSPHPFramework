ec5449eb071bd8640fbf461962b26a97e4d7195b
nemodaattila <nemodaattila@gmail.com>
2024-05-31 13:42:44 +0200

    renamed buttonTable parameter to ButtonContainer

--- frontend/modules/dataHandlerTable/HandlerViewParent.js
+++ frontend/modules/dataHandlerTable/HandlerViewParent.js
@@ -1,8 +1,10 @@
 class HandlerViewParent extends ViewParent{
 
-    addButtonToTallTable(columnNum, buttonParams, onclickEvent, sameLine = false) {
-        if (this.buttonTable === undefined)
-            this.buttonTable = HtmlElementCreator.createSimpleHtmlElement('div', this._windowContentMainContainer, {class: 'buttonTable'})
+    _buttonContainer
+
+    addButtonContainer(columnNum, buttonParams, onclickEvent, sameLine = false) {
+        if (this._buttonContainer === undefined)
+            this._buttonContainer = HtmlElementCreator.createSimpleHtmlElement('div', this._windowContentMainContainer, {class: 'buttonContainer'})
         //DO columncount
         // let columnCount = (this.windowContentPointer.content.inputPerRow ?? 1) * 2
         let columnCount=2
@@ -11,14 +13,14 @@ class HandlerViewParent extends ViewParent{
         for (let i = 0; i < columnCount; i++)
             cols += ' 262px '
         let tds = [];
-        let tr = sameLine === true && this.buttonTable.childNodes.length !== 0 ? this.buttonTable.lastChild : HtmlElementCreator.createSimpleHtmlElement('div', this.buttonTable)
+        let tr = sameLine === true && this._buttonContainer.childNodes.length !== 0 ? this._buttonContainer.lastChild : HtmlElementCreator.createSimpleHtmlElement('div', this._buttonContainer)
         if (!sameLine)
             tr.style.gridTemplateColumns += cols
         if (sameLine === false) {
             tr.style.display = 'grid'
             for (let i = 0; i < columnCount; i++)
                 tds.push(HtmlElementCreator.createSimpleHtmlElement('div', tr, {class: 'tableCell'}))
-        } else tds = this.buttonTable.lastChild.children
+        } else tds = this._buttonContainer.lastChild.children
         buttonParams.type = 'button';
         console.log(tds);
         (HtmlElementCreator.createSimpleHtmlElement('input', tds[columnNum], buttonParams)).addEventListener('click', onclickEvent)
