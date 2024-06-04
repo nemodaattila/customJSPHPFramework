245f3ae8262683dc64ea476ef503d230b9a46d9e
nemodaattila <nemodaattila@gmail.com>
2024-06-04 16:16:31 +0200

    added EntityHandler filesToInclude.js

---
+++ frontend/modules/entityHandler/filesToInclude.js
@@ -0,0 +1,60 @@
+Includer.setIncludableFileSource(
+    'dataHandlerTable', [{
+        directory: MODULE_FILE_DIR + 'dataHandlerTable',
+        fileNames: [
+            'HandlerTable.js',
+            'HandlerTableView.js',
+            'HandlerTable.css',
+            // 'SearchAndOrderParameters.js',
+            'HandlerControllerParent.js',
+            'HandlerViewParent.js'
+        ]
+    }])
+Includer.setIncludableFileSource(
+    'listerTable', [
+        {
+            directory: MODULE_FILE_DIR + 'dataListerTable',
+            fileNames: [
+                'ListerTable.js',
+                'ListerTableView.js',
+                'ListerTable.css',
+                'SearchAndOrderParameters.js',
+                'ListerControllerParent.js'
+            ]
+        }])
+Includer.setIncludableFileSource(
+    'pageTurner', [
+        {
+            directory: MODULE_FILE_DIR + 'dataListerTable',
+            fileNames: [
+                'PageTurnerController.js',
+                'PageTurner.css',
+            ]
+        }])
+Includer.setIncludableFileSource(
+    //DO rename
+    'listerTableSearchConnector', [
+        {
+            directory: MODULE_FILE_DIR + 'dataListerTable',
+            fileNames: [
+                'ListerTableSearchConnector.js',
+            ]
+        }])
+Includer.setIncludableFileSource(
+    'infinityScroller', [
+        {
+            directory: MODULE_FILE_DIR + 'dataListerTable',
+            fileNames: [
+                'InfinityScrollerController.js',
+                'InfinityScroller.css',
+            ]
+        }])
+Includer.setIncludableFileSource(
+    'dataServiceParent', [
+        {
+            directory: MODULE_FILE_DIR + 'dataServiceParent',
+            fileNames: [
+                'ServiceParent.js',
+                'DataServiceModelParent.js',
+            ]
+        }])
