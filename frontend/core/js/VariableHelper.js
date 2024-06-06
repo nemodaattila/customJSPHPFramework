5e97b7336da34680bc52e7cd23e281afe0e32e2f
nemodaattila <nemodaattila@gmail.com>
2024-06-06 13:40:40 +0200

    added VariableHelper.js

---
+++ frontend/core/js/VariableHelper.js
@@ -0,0 +1,13 @@
+/**
+ * assistant class for variables
+ */
+class Class {
+    /**
+     *  returns true if parameter "str" is string, false if not
+     */
+    static isString(str)
+    {
+        return  (typeof str === 'string') || (str instanceof String)
+    }
+}
+
