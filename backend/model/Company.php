aaf6b1409491b3e2c16ba91cc9c22fb7c244e622
nemodaattila <nemodaattila@gmail.com>
2024-06-07 13:38:31 +0200

    model/Company.php and RecordUpdater.php added

---
+++ backend/model/Company.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace model;
+class Company
+{
+   private int $id;
+   private string $name;
+
+    private string $address;
+
+    private string $vat_number;
+    private int $category;
+
+    private string $comment;
+
+
+
+}
