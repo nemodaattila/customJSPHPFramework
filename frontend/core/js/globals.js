303c5cf05cc86e3a1f8e48d447f99c81c804fd8b
nemodaattila <nemodaattila@gmail.com>
2024-06-11 20:02:24 +0200

    added PAGE_TURNER_TYPES global variable

--- frontend/core/js/globals.js
+++ frontend/core/js/globals.js
@@ -3,5 +3,6 @@ const BACKEND_IMAGE_URL = '../backend/image/'
 const CORE_FILE_DIR = './core/js'
 const MODULE_FILE_DIR = './modules/'
 const WEB_DESKTOP_MODULE_DIR = MODULE_FILE_DIR + 'webdesktop/'
+const PAGE_TURNER_TYPES = ['infinityScroller', 'pageTurner']
 
 
