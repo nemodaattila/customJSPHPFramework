bad1bf3ccfb6115eca200cb30bbcc903b49bbb54
nemodaattila <nemodaattila@gmail.com>
2024-06-14 18:06:23 +0200

    corrected bug: after service init model's loaded attribute wasn't set to true

--- frontend/modules/companies/CompanyService.js
+++ frontend/modules/companies/CompanyService.js
@@ -9,9 +9,12 @@ class CompanyService extends EntityServiceControllerParent {
      */
     async init() {
         return new Promise(async (resolve) => {
-            if (this._model.loaded)
+            if (this._model.loaded === true) {
                 resolve(true)
+                return
+            }
             await this.getMetaParameters()
+            this._model.loaded = true
             resolve(await super.init())
         })
     }
