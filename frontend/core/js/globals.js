8da621bfe3842e73e6add22ca6e1ff724ff30bf0
nemodaattila <nemodaattila@gmail.com>
2024-06-25 17:19:53 +0200

    added 'DateTableInput' as available Table Input Class

--- frontend/core/js/globals.js
+++ frontend/core/js/globals.js
@@ -4,6 +4,6 @@ const CORE_FILE_DIR = './core/js'
 const MODULE_FILE_DIR = './modules/'
 const WEB_DESKTOP_MODULE_DIR = MODULE_FILE_DIR + 'webdesktop/'
 const PAGE_TURNER_TYPES = ['infinityScroller', 'pageTurner']
-const ENTITY_HANDLER_INPUT_TYPES = ['NumberTableInput','StringTableInput','SelectTableInput']
+const ENTITY_HANDLER_INPUT_TYPES = ['NumberTableInput','StringTableInput','SelectTableInput','DateTableInput']
 
 
